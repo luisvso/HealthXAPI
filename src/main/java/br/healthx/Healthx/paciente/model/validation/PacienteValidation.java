de4a38c960728d74f2aef588339e1ecba74b6304
luisvso <luisvso.dev@gmail.com>
2025-09-18 18:35:49 -0300

    feat(PacienteValidation): adicionado a funcionalidade de validar nome no PacienteValidation, para impedir que o nome seja null

--- src/main/java/br/healthx/Healthx/paciente/model/validation/PacienteValidation.java
+++ src/main/java/br/healthx/Healthx/paciente/model/validation/PacienteValidation.java
@@ -1,14 +1,23 @@
 package br.healthx.Healthx.paciente.model.validation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import br.healthx.Healthx.paciente.dto.PacienteDTO;
+import br.healthx.Healthx.paciente.model.repository.PacienteRepository;
 
 @Configuration
 public class PacienteValidation {
 
+    @Autowired
+    PacienteRepository pacienteRepository;
+
     public boolean existId(PacienteDTO dto) {
         return dto.id() != null;
+    }
+
+    public boolean existName(String name){
+        return name != null;
     }
 
 }
