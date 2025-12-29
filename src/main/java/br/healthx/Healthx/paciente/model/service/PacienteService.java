c37b6b8dea0fccc2d93b485cd53927d3133394cc
Luis Valdo <luisvso.dev@gmail.com>
2025-12-28 23:31:59 -0300

    refactor(model): changed the files to implement the mapper and the new DTOs

--- src/main/java/br/healthx/Healthx/paciente/model/service/PacienteService.java
+++ src/main/java/br/healthx/Healthx/paciente/model/service/PacienteService.java
@@ -3,9 +3,15 @@ package br.healthx.Healthx.paciente.model.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import br.healthx.Healthx.paciente.dto.PacienteDTO;
+import com.sun.net.httpserver.Request;
+
+import br.healthx.Healthx.paciente.dto.RequestPacienteDTO;
+import br.healthx.Healthx.paciente.dto.ResponsePacienteDTO;
+import br.healthx.Healthx.paciente.mapper.PacienteMapper;
 import br.healthx.Healthx.paciente.model.entity.Paciente;
 import br.healthx.Healthx.paciente.model.repository.PacienteRepository;
 import br.healthx.Healthx.paciente.model.validation.PacienteValidation;
@@ -13,54 +19,65 @@ import br.healthx.Healthx.paciente.model.validation.PacienteValidation;
 @Service
 public class PacienteService {
 
-    private PacienteRepository pacienteRepository;
-    private PacienteValidation pacienteValidation;
+    private final PacienteRepository pacienteRepository;
+    private final PacienteValidation pacienteValidation;
+    private final PacienteMapper pacienteMapper;
 
-    public PacienteService(PacienteRepository pacienteRepository, PacienteValidation pacienteValidation) {
+    public PacienteService(PacienteRepository pacienteRepository, PacienteValidation pacienteValidation,
+            PacienteMapper pacienteMapper) {
         this.pacienteRepository = pacienteRepository;
         this.pacienteValidation = pacienteValidation;
+        this.pacienteMapper = pacienteMapper;
     }
 
-    public List<Paciente> createListPaciente(List<PacienteDTO> dtos) {
-        List<Paciente> pacientes = new ArrayList<Paciente>();
-        for (PacienteDTO dto : dtos)
-            pacientes.add(create(dto));
-        return pacientes;
-    }
+    // public List<Paciente> createListPaciente(List<RequestPacienteDTO> dtos) {
+    // List<Paciente> pacientes = new ArrayList<Paciente>();
+    // for (RequestPacienteDTO dto : dtos)
+    // pacientes.add(create(dto));
+    // return pacientes;
+    // }
 
     /// method for creating Paciente
-    public Paciente create(PacienteDTO dto) {
-        return pacienteRepository.save(new Paciente(dto));
+    public ResponsePacienteDTO create(RequestPacienteDTO dto) {
+        Paciente paciente = pacienteMapper.toEntity(dto);
+        paciente = pacienteRepository.save(paciente);
+
+        return pacienteMapper.entityToResponse(paciente);
+
     }
 
-    public String delete(PacienteDTO dto) {
-        if (pacienteValidation.existId(dto))
-            pacienteRepository.delete(new Paciente(dto));
-            return "User deletado";
+    public void delete(Long id) {
+        pacienteValidation.existId(id);
+        pacienteRepository.deleteById(id);
     }
 
-    public Paciente update(PacienteDTO dto) {
-        return (pacienteValidation.existId(dto) ? create(dto) : null);
+    public ResponsePacienteDTO update(Long id, RequestPacienteDTO dto) {
+        Paciente paciente = pacienteRepository.findById(id).get();
+
+        paciente.setName(dto.name());
+        paciente.setEmail(dto.email());
+        paciente.setDataNascimento(dto.dataNascimento());
+        paciente.setGenero(dto.genero());
+        paciente.setTelefone(dto.telefone());
+        paciente.setResponsavel(dto.responsavel());
+        paciente.setRaca(dto.raca());
+
+        paciente = pacienteRepository.save(paciente);
+
+        return pacienteMapper.entityToResponse(paciente);
+
     }
 
-    public Paciente find(PacienteDTO dto) {
-        return (pacienteValidation.existId(dto) ? pacienteRepository.findById(dto.id()).get() : null);
+    public ResponsePacienteDTO findById(Long id) {
+        pacienteValidation.existId(id);
+        return pacienteMapper.entityToResponse(pacienteRepository.findById(id).get());
     }
 
-    public List<Paciente> findByNome(String nome){
-        List<Paciente> pacientes = pacienteRepository.findAll();
-        List<Paciente> nomeAchados = new ArrayList<Paciente>();
-
-        if(pacienteValidation.existName(nome)){
-            for (Paciente paciente : pacientes) {
-                if(nome.toLowerCase().equals(paciente.getNome().toLowerCase()))
-                nomeAchados.add(paciente);
-            }
-        }
-            return nomeAchados;
+    public List<ResponsePacienteDTO> findByName(String name) {
+        return pacienteMapper.toResponseList(pacienteRepository.findByNameContainingIgnoreCase(name));
     }
 
-    public List<Paciente> findAll() {
-        return pacienteRepository.findAll();
+    public List<ResponsePacienteDTO> findAll() {
+        return pacienteMapper.toResponseList(pacienteRepository.findAll());
     }
 }
