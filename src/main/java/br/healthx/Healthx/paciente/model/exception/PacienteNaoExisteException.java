f7744e2b23fecd2b828847e433f02c895dc2b15a
Luis Valdo <luisvso.dev@gmail.com>
2025-12-29 17:12:06 -0300

    del: deleted the pacienteNaoExisteException because it was not necessery anymore

--- src/main/java/br/healthx/Healthx/paciente/model/exception/PacienteNaoExisteException.java
+++
@@ -1,7 +0,0 @@
-package br.healthx.Healthx.paciente.model.exception;
-
-public class PacienteNaoExisteException extends RuntimeException {
-    public PacienteNaoExisteException(String message) {
-        super(message);
-    }
-}
