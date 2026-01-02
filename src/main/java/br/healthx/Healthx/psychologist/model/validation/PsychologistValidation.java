233777c6a50e0b6aead3bc72d0f1c63564821c8a
Luis Valdo <luisvso.dev@gmail.com>
2026-01-01 23:39:20 -0300

    feat(Validation): Added the basic validation of email, and user of psychologists

---
+++ src/main/java/br/healthx/Healthx/psychologist/model/validation/PsychologistValidation.java
@@ -0,0 +1,29 @@
+package br.healthx.Healthx.psychologist.model.validation;
+
+import org.springframework.stereotype.Component;
+
+import br.healthx.Healthx.psychologist.dto.PsychologistRequestDTO;
+import br.healthx.Healthx.psychologist.model.entity.Psychologist;
+import br.healthx.Healthx.psychologist.model.exception.PsychologistAlreadyExistsException;
+import br.healthx.Healthx.psychologist.model.repository.PsychologistRepository;
+
+@Component
+public class PsychologistValidation {
+
+    private final PsychologistRepository psychologistRepository;
+
+    public PsychologistValidation(PsychologistRepository psychologistRepository) {
+        this.psychologistRepository = psychologistRepository;
+    }
+
+    public void validatePsychologist(PsychologistRequestDTO dto) {
+        alreadyExistsPsychologist(dto);
+    }
+
+    public void alreadyExistsPsychologist(PsychologistRequestDTO dto) {
+        for (Psychologist psychologist : psychologistRepository.findAll()) {
+            if (dto.email().equals(psychologist.getEmail()) && !dto.CRP().equals(psychologist.getCRP()))
+                throw new PsychologistAlreadyExistsException("This email is already registered");
+        }
+    }
+}
