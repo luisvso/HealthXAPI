69e48cf3abc54949984662642084728272b386b0
Luis Valdo <luisvso.dev@gmail.com>
2026-01-01 23:35:25 -0300

    feat(Model): Added the psychologist entity

---
+++ src/main/java/br/healthx/Healthx/psychologist/model/entity/Psychologist.java
@@ -0,0 +1,45 @@
+package br.healthx.Healthx.psychologist.model.entity;
+
+import br.healthx.Healthx.psychologist.dto.PsychologistRequestDTO;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Psychologist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Your Email must not be blank")
+    private String email;
+
+    @NotBlank(message = "The Phone")
+    private String phone;
+
+    @NotBlank(message = "The name must not be blank")
+    private String name;
+
+    @NotBlank(message = "The CRP must not be blank")
+    private String CRP;
+
+    public Psychologist() {
+    }
+
+    public Psychologist(PsychologistRequestDTO dto) {
+        this.email = dto.email();
+        this.phone = dto.phone();
+        this.name = dto.name();
+        this.CRP = dto.CRP();
+    }
+
+}
