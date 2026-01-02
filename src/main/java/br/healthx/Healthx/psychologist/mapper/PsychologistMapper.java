d22038770074daa42c02db478f7e175821df4178
Luis Valdo <luisvso.dev@gmail.com>
2026-01-01 23:41:43 -0300

    feat(Mapper): Added the mapper module for converting dto to entity and vice versa

---
+++ src/main/java/br/healthx/Healthx/psychologist/mapper/PsychologistMapper.java
@@ -0,0 +1,43 @@
+package br.healthx.Healthx.psychologist.mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import br.healthx.Healthx.psychologist.dto.PsychologistRequestDTO;
+import br.healthx.Healthx.psychologist.model.entity.Psychologist;
+
+@Component
+public class PsychologistMapper {
+
+    public PsychologistRequestDTO psychologistToDTO(Psychologist psychologist) {
+        Psychologist psychologist2 = new Psychologist();
+        psychologist2.setEmail(psychologist.getEmail());
+        psychologist2.setCRP(psychologist.getCRP());
+        psychologist2.setName(psychologist.getName());
+        psychologist2.setPhone(psychologist.getPhone());
+
+        return new PsychologistRequestDTO(psychologist2.getEmail(), psychologist2.getPhone(), psychologist2.getName(),
+                psychologist2.getCRP());
+    }
+
+    public Psychologist dtoToPsychologist(PsychologistRequestDTO dto) {
+        Psychologist psychologist = new Psychologist();
+        psychologist.setCRP(dto.CRP());
+        psychologist.setEmail(dto.email());
+        psychologist.setName(dto.name());
+        psychologist.setPhone(dto.phone());
+
+        return psychologist;
+    }
+
+    public List<PsychologistRequestDTO> listToPsychologistDTO(List<Psychologist> psychologists) {
+        List<PsychologistRequestDTO> psychologistList = psychologists.stream()
+                .map(this::psychologistToDTO)
+                .collect(Collectors.toList());
+
+        return psychologistList;
+    }
+
+}
