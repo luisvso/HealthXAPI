83666cddba759386bdd3880677a202552fa18f17
Luis Valdo <luisvso.dev@gmail.com>
2026-01-09 21:37:52 -0300

    feat(Validation): Added the Validation for StartDate not be after endDate

---
+++ src/main/java/br/healthx/Healthx/session/model/validation/SessionValidation.java
@@ -0,0 +1,22 @@
+package br.healthx.Healthx.session.model.validation;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Component;
+
+import br.healthx.Healthx.session.dto.SessionRequestDTO;
+import br.healthx.Healthx.session.model.exception.SessionDateException;
+
+@Component
+public class SessionValidation {
+
+    public void validateSessionCreation(SessionRequestDTO dto) {
+        validateDates(dto);
+    }
+
+    private void validateDates(SessionRequestDTO dto) {
+        if (dto.startDate().isBefore(LocalDate.now()) || dto.endDate().isBefore(dto.startDate())) {
+            throw new SessionDateException("The date is invalid");
+        }
+    }
+}
