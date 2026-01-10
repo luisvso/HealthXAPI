28b16d321afcec4edc97a2a975ee95b17175ed7c
Luis Valdo <luisvso.dev@gmail.com>
2026-01-09 21:36:28 -0300

    feat(Exception): Added the exception for sessions

---
+++ src/main/java/br/healthx/Healthx/session/model/exception/SessionDateException.java
@@ -0,0 +1,7 @@
+package br.healthx.Healthx.session.model.exception;
+
+public class SessionDateException extends RuntimeException {
+    public SessionDateException(String message) {
+        super(message);
+    }
+}
