b651c20b3125e251c18b68325b5b167b0645c3b4
Luis Valdo <luisvso.dev@gmail.com>
2026-01-10 23:52:25 -0300

    feat(Repository): Added the methods to repository class to filter between dates, filter status, sessionType and per name

--- src/main/java/br/healthx/Healthx/session/model/repository/SessionRepository.java
+++ src/main/java/br/healthx/Healthx/session/model/repository/SessionRepository.java
@@ -1,11 +1,23 @@
 package br.healthx.Healthx.session.model.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.healthx.Healthx.session.model.entity.Session;
+import br.healthx.Healthx.session.model.entity.SessionType;
+import br.healthx.Healthx.session.model.entity.Status;
 
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Long> {
 
+    public List<Session> findByStatus(Status status);
+
+    public List<Session> findBySessionType(SessionType sessionType);
+
+    public List<Session> findByPatient_NameContainingIgnoreCase(String name);
+
+    public List<Session> findByStartDateBetween(LocalDate start, LocalDate end);
 }
