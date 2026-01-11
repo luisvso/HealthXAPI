d0426e527fc851733cd91ac05ee84010fa70975f
Luis Valdo <luisvso.dev@gmail.com>
2026-01-11 17:21:47 -0300

    feat(model): created exception for session not found trying to get from the database

---
+++ src/main/java/br/healthx/Healthx/session/model/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package br.healthx.Healthx.session.model.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super();
+    }
+}
