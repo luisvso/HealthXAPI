669b6704d36b59559d24f230f4fb1003196253cf
Luis Valdo <luisvso.dev@gmail.com>
2026-01-16 19:06:13 -0300

    feat(Handler): Added to the globalHandler new Exceptions to handle

--- src/main/java/br/healthx/Healthx/session/handler/GlobalExceptionHandler.java
+++ src/main/java/br/healthx/Healthx/session/handler/GlobalExceptionHandler.java
@@ -1,28 +1,36 @@
 package br.healthx.Healthx.session.handler;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import br.healthx.Healthx.session.dto.ErrorResponse;
 import br.healthx.Healthx.session.model.exception.ResourceNotFoundException;
 import br.healthx.Healthx.session.model.exception.SessionDateException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
 
-@ControllerAdvice
+// RestControllerAdvice combines ControllerAdvice + ResponseBody 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(SessionDateException.class)
     public ResponseEntity<ErrorResponse> handleSessionDate(SessionDateException ex, HttpServletRequest servletRequest) {
+
         ErrorResponse errorResponse = new ErrorResponse(
                 HttpStatus.CONFLICT.value(),
                 HttpStatus.CONFLICT.getReasonPhrase(),
                 ex.getMessage(),
                 servletRequest.getRequestURI(),
+                Collections.singletonMap(" ", ex.getMessage()),
                 LocalDateTime.now());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
@@ -37,9 +45,76 @@ public class GlobalExceptionHandler {
                 HttpStatus.NOT_FOUND.getReasonPhrase(),
                 ex.getMessage(),
                 httpServletRequest.getRequestURI(),
+                Collections.singletonMap(" ", ex.getMessage()),
                 LocalDateTime.now());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodNotValid(MethodArgumentNotValidException ex,
+            HttpServletRequest httpServletRequest) {
+
+        Map<String, String> fieldErrors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors()
+                .forEach(error -> fieldErrors.put(error.getObjectName(), error.getDefaultMessage()));
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                ex.getMessage(),
+                httpServletRequest.getRequestURI(),
+                fieldErrors,
+                LocalDateTime.now());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleDataIntegridy(DataIntegrityViolationException ex,
+            HttpServletRequest httpServletRequest) {
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
+                ex.getMessage(),
+                httpServletRequest.getRequestURI(),
+                Collections.singletonMap(" ", ex.getMessage()),
+                LocalDateTime.now());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolation(ConstraintViolationException ex,
+            HttpServletRequest htp) {
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
+                ex.getMessage(),
+                htp.getRequestURI(),
+                Collections.singletonMap(" ", ex.getMessage()),
+                LocalDateTime.now());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArguments(IllegalArgumentException ex, HttpServletRequest htp) {
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                ex.getMessage(),
+                htp.getRequestURI(),
+                Collections.singletonMap(" ", ex.getMessage()),
+                LocalDateTime.now()
+
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
@@ -50,6 +125,7 @@ public class GlobalExceptionHandler {
                 HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
                 "Internal server error",
                 httpServletRequest.getRequestURI(),
+                Collections.singletonMap(" ", ex.getMessage()),
                 LocalDateTime.now());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
