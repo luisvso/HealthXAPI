2469e007ae66a4810f74485ddb9619ae51416962
Luis Valdo <luisvso.dev@gmail.com>
2026-01-19 00:28:11 -0300

    feat(PacienteExceptionNotFoundException): The message was modified to make the reason of the erro more clear

--- src/main/java/br/healthx/Healthx/paciente/model/exception/PacienteNotFoundException.java
+++ src/main/java/br/healthx/Healthx/paciente/model/exception/PacienteNotFoundException.java
@@ -1,7 +1,7 @@
 package br.healthx.Healthx.paciente.model.exception;
 
 public class PacienteNotFoundException extends RuntimeException {
-    public PacienteNotFoundException(String message) {
-        super(message);
+    public PacienteNotFoundException(Long id) {
+        super("The patient of id " + id + " does not exits");
     }
 }
