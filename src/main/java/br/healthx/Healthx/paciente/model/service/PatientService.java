87a7249f19f75cc9c26b93cbdb52632a390c633d
Luis Valdo <luisvso.dev@gmail.com>
2026-01-19 22:19:02 -0300

    feat(Paciente/Service): Added the logs for patient service class

--- src/main/java/br/healthx/Healthx/paciente/model/service/PatientService.java
+++ src/main/java/br/healthx/Healthx/paciente/model/service/PatientService.java
@@ -32,22 +32,33 @@ public class PatientService {
     /// method for creating Paciente
     @Transactional(timeout = 30)
     public ResponsePatientDTO create(RequestPatientDTO dto) {
+
+        log.info("Attempt to create an Patient");
+
         patientValidation.validatingPatient(dto);
         Patient paciente = patientMapper.toEntity(dto);
         paciente = patientRepository.save(paciente);
 
+        log.info("Patient of id: {} was created and saved on the dabatase", paciente.getId());
         return patientMapper.entityToResponse(paciente);
 
     }
 
     @Transactional(timeout = 30)
     public void delete(Long id) {
+
+        log.info("The method for deleting a patient was started");
+
         patientValidation.existId(id);
         patientRepository.deleteById(id);
+
+        log.info("Patient of id : {} was deleted sucessfully", id);
     }
 
     @Transactional(timeout = 30)
     public ResponsePatientDTO update(Long id, RequestPatientDTO dto) {
+        log.info("The method for updating the patient was started");
+
         Patient patient = patientRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("The Patient of id: " + id + " was not found"));
 
@@ -61,12 +72,16 @@ public class PatientService {
 
         patient = patientRepository.save(patient);
 
+        log.info("The patient of id: {} was updated and saved on the database sucessfully", id);
+
         return patientMapper.entityToResponse(patient);
 
     }
 
     @Transactional(readOnly = true, timeout = 15)
     public ResponsePatientDTO findById(Long id) {
+        log.info("The method for find the id : {} was started", id);
+
         patientValidation.existId(id);
         return patientMapper.entityToResponse(patientRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("The patient of id" + id + " was not found")));
@@ -74,11 +89,13 @@ public class PatientService {
 
     @Transactional(readOnly = true, timeout = 15)
     public Page<ResponsePatientDTO> findByName(String name, Pageable pageable) {
+        log.info("The method for finding an patient of name: {} was started", name);
         return patientMapper.toResponseList(patientRepository.findByNameContainingIgnoreCase(name, pageable));
     }
 
     @Transactional(readOnly = true, timeout = 15)
     public Page<ResponsePatientDTO> findAll(Pageable pageable) {
+        log.info("The method for fetching all patients was started");
         return patientMapper.toResponseList(patientRepository.findAll(pageable));
     }
 }
