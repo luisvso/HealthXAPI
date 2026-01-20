5cf8b1f464cb58a50ca7077582743e9849446cd7
Luis Valdo <luisvso.dev@gmail.com>
2026-01-19 23:44:15 -0300

    feat(PsychologistService): Added the transactional annotation to the PsychologistService class, to improve database operations more reliable and consistent

--- src/main/java/br/healthx/Healthx/psychologist/model/service/PsychologistService.java
+++ src/main/java/br/healthx/Healthx/psychologist/model/service/PsychologistService.java
@@ -2,7 +2,9 @@ package br.healthx.Healthx.psychologist.model.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.healthx.Healthx.psychologist.dto.PsychologistRequestDTO;
 import br.healthx.Healthx.psychologist.mapper.PsychologistMapper;
@@ -32,6 +34,7 @@ public class PsychologistService {
         this.psychologistValidation = psychologistValidation;
     }
 
+    @Transactional(timeout = 30)
     public PsychologistRequestDTO create(PsychologistRequestDTO dto) {
         psychologistValidation.validatePsychologist(dto);
 
@@ -42,6 +45,7 @@ public class PsychologistService {
         return psychologistMapper.psychologistToDTO(psy);
     }
 
+    @Transactional(timeout = 30)
     public PsychologistRequestDTO update(Long id, PsychologistRequestDTO dto) {
         psychologistValidation.validatePsychologist(dto);
         Psychologist psy = psychologistRepository.findById(id).get();
@@ -56,15 +60,18 @@ public class PsychologistService {
         return psychologistMapper.psychologistToDTO(psy);
     }
 
+    @Transactional(timeout = 30)
     public void delete(Long id) {
         psychologistRepository.deleteById(id);
     }
 
+    @Transactional(readOnly = true, timeout = 15)
     public PsychologistRequestDTO findOne(Long id) {
         Psychologist psychologist = psychologistRepository.findById(id).get();
         return psychologistMapper.psychologistToDTO(psychologist);
     }
 
+    @Transactional(readOnly = true, timeout = 15)
     public List<PsychologistRequestDTO> listAll() {
         return psychologistMapper.listToPsychologistDTO(psychologistRepository.findAll());
     }
