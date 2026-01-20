e1f274a7d1b588174126b1d621aac838ee2d7c65
Luis Valdo <luisvso.dev@gmail.com>
2026-01-19 23:40:54 -0300

    feat(psychologist/dto): Added the validation annotations for field inside the DTO

--- src/main/java/br/healthx/Healthx/psychologist/dto/PsychologistRequestDTO.java
+++ src/main/java/br/healthx/Healthx/psychologist/dto/PsychologistRequestDTO.java
@@ -1,7 +1,11 @@
 package br.healthx.Healthx.psychologist.dto;
 
-public record PsychologistRequestDTO(String email,
-        String phone,
-        String name,
-        String CRP) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record PsychologistRequestDTO(
+        @Email(message = "Invalid email format") @NotBlank(message = "Your Email must not be blank") String email,
+        @NotBlank(message = "The Phone") String phone,
+        @NotBlank(message = "The name must not be blank") String name,
+        @NotBlank(message = "The CRP must not be blank") String CRP) {
 }
