cf2cfd39081fda53d9ef7e103a7cf21e67e0d648
Luis Valdo <luisvso.dev@gmail.com>
2026-01-19 23:10:43 -0300

    feat(paciente/dto): Added the validation annotations for the DTO fields on request and responseDTOs

--- src/main/java/br/healthx/Healthx/paciente/dto/ResponsePatientDTO.java
+++ src/main/java/br/healthx/Healthx/paciente/dto/ResponsePatientDTO.java
@@ -4,14 +4,17 @@ import java.time.LocalDate;
 
 import br.healthx.Healthx.paciente.model.entity.Gender;
 import br.healthx.Healthx.paciente.model.entity.Race;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 
 public record ResponsePatientDTO(
-        String name,
-        LocalDate birthDate,
-        Gender gender,
+        @NotNull(message = "Your name should not be null") String name,
+        @NotNull(message = "Your birthDate should not be null") LocalDate birthDate,
+        @NotNull(message = "This filed should not be null") Gender gender,
         String guardian,
-        Race race,
-        String email,
-        String phone,
+        @NotNull(message = "This field should not be null") Race race,
+        @NotNull(message = "Your email should not be null") @Email(message = "Invalid Email format") String email,
+        @NotNull(message = "The phone should not be null") @Size(max = 15, message = "The phone number must hava at most 15 digits") String phone,
         LocalDate createAt) {
 }
