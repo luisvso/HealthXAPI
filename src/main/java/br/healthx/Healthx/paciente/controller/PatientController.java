941d8b7e5aae992a10b4b75cbbd21659365f961e
Luis Valdo <luisvso.dev@gmail.com>
2026-01-29 00:11:18 -0300

    feat(PatientController): Added the Patient documentation end-points

--- src/main/java/br/healthx/Healthx/paciente/controller/PatientController.java
+++ src/main/java/br/healthx/Healthx/paciente/controller/PatientController.java
@@ -14,8 +14,11 @@ import jakarta.validation.Valid;
 import br.healthx.Healthx.paciente.dto.RequestPatientDTO;
 import br.healthx.Healthx.paciente.dto.ResponsePatientDTO;
 import br.healthx.Healthx.paciente.model.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@Tag(name = "Patient", description = "This is the endpoint for patients managment")
 @RequestMapping("/paciente")
 public class PatientController {
 
@@ -26,42 +29,42 @@ public class PatientController {
     }
 
     @PostMapping
+    @Operation(summary = "Create a new Patient", description = "This methods creates a new Patient and register on the database")
     public ResponseEntity<ResponsePatientDTO> add(@RequestBody @Valid RequestPatientDTO dto) {
         ResponsePatientDTO responsePacienteDTO = patientService.create(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responsePacienteDTO);
     }
 
-    // @PostMapping("/addAll")
-    // public ResponseEntity<List<Paciente>> addAll(@RequestBody @Valid
-    // List<PacienteDTO> dtos) {
-    // return ResponseEntity.ok(pacienteService.createListPaciente(dtos));
-    // }
-
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a Patient", description = "Delete a patient that is previously registered on the system database")
     public void delete(@PathVariable("id") Long id) {
         patientService.delete(id);
 
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Find one Patient", description = "Finds one Patient using their Id")
     public ResponseEntity<ResponsePatientDTO> findOne(@PathVariable("id") Long id) {
         ResponsePatientDTO responsePacienteDTO = patientService.findById(id);
         return ResponseEntity.ok(responsePacienteDTO);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Upadte a Patient", description = "Upadtes a Patient that is registered previously on the system")
     public ResponseEntity<ResponsePatientDTO> update(@RequestBody @Valid RequestPatientDTO dto,
             @PathVariable("id") Long id) {
         return ResponseEntity.ok(patientService.update(id, dto));
     }
 
     @GetMapping
+    @Operation(summary = "Find all Patients", description = "Find all the patients registered on the system")
     public ResponseEntity<Page<ResponsePatientDTO>> findAll(
             @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
         return ResponseEntity.ok(patientService.findAll(pageable));
     }
 
     @GetMapping("/name/{name}")
+    @Operation(summary = "Find a patient by their name", description = "Find a Patient passing their name as a parameter")
     public ResponseEntity<Page<ResponsePatientDTO>> findName(@PathVariable("name") String name,
             @PageableDefault(size = 10, sort = "name", direction = Sort.Direction.ASC) Pageable pageable) {
         return ResponseEntity.ok(patientService.findByName(name, pageable));
