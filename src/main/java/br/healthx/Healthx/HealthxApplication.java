3d62091c32b7b2c073fb1050ab2bb0fcf318f241
Luis Valdo <luisvso.dev@gmail.com>
2026-01-28 23:21:57 -0300

    feat(Main): The Swagger documentation was allowed

--- src/main/java/br/healthx/Healthx/HealthxApplication.java
+++ src/main/java/br/healthx/Healthx/HealthxApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@OpenAPIDefinition(info = @Info(title = "Psychologist sessions managment API", version = "1.0", description = "API documentation for managment of psychologist sessions"))
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class HealthxApplication {
