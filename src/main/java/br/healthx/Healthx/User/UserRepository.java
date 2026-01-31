e42419e8fca0e0c01cb965416c1a286e8f0aed80
Luis Valdo <luisvso.dev@gmail.com>
2026-01-30 23:41:30 -0300

    refactor(User): The User creating was modified to fit with the new parameter received

--- src/main/java/br/healthx/Healthx/User/UserRepository.java
+++ src/main/java/br/healthx/Healthx/User/UserRepository.java
@@ -9,5 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional <User> findByLogin(String login);
+    Optional<User> findByLogin(String login);
+
+    Optional<User> findById(Long id);
 }
